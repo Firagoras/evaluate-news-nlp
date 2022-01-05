062940b2ad254d0d18939deae28129328309d2e6
Firagoras <firagoras@gmail.com>
2022-01-05 23:04:05 +0100

    refactor: refactoring

--- src/client/index.js
+++ src/client/index.js
@@ -1,43 +1,6 @@
-import './styles/base.scss'
+import './styles/base.scss';
+import { postData } from './js/formHandler.js';
 
-const srcUrl = document.getElementById('urlInput');
 const form = document.getElementById('form');
 
-function updateUI(data) {
-  document.getElementById('resultsHeadline').innerHTML = '<h2>Analysis Results</h2>'
-  document.getElementById('srcUrl').innerHTML = `Source: <a href="${data.srcUrl}" target="_blank">${data.srcUrl}</a>`;
-  document.getElementById('agreement').innerHTML = `Agreement: ${data.agreement}`;
-  document.getElementById('confidence').innerHTML = `Confidence: ${data.confidence}`;
-  document.getElementById('irony').innerHTML = `Irony: ${data.irony}`;
-  document.getElementById('scoreTag').innerHTML = `Score Tag: ${data.scoreTag}`;
-  document.getElementById('subjectivity').innerHTML = `Subjectivity: ${data.subjectivity}`;
-}
-
-// Asynchronous function to make a POST request to the server
-const postData = async (e) => {
-  e.preventDefault();
-  const data = {
-    "textUrl": srcUrl.value
-  };
-
-  const response = await fetch('/postData', {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-
-  try {
-    const serverData = await response.json();
-    console.log(serverData);
-    updateUI(serverData);
-    return serverData;
-  } catch (error) {
-    console.log('error', error);
-  }
-}
-
-// generateBtn.addEventListener('click', postData);
 form.addEventListener('submit', postData);
