42f26d85886988df1f5130431fe20683548cb4b6
Firagoras <firagoras@gmail.com>
2022-01-17 11:35:34 +0100

    update tests

--- __test__/formHandler.test.js
+++ __test__/formHandler.test.js
@@ -1,7 +1,12 @@
-import { handleSubmit } from '../src/client/js/formHandler.js';
+// import { handleSubmit, postData } from '../src/client/js/formHandler.js';
+const { handleSubmit, postData } = require('../src/client/js/formHandler');
   
 describe("Testing the submit functionality", () => {
   test("Testing the handleSubmit() function", () => {
     expect(handleSubmit).toBeDefined();
+  })
+
+  test("Testing the postData() function", () => {
+    expect(postData).toBeDefined();
   })
 });
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -48,4 +48,4 @@ function handleSubmit(e) {
   }
 }
 
-export { handleSubmit };
+export { handleSubmit, postData };
